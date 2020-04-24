ac697b3e1fc442b808d60b21ef87960cdfa88e64
TJGorrie <tyler.gorrie-stone@diamond.ac.uk>
2020-04-24 10:21:25 +0100

    Configuring recontain

--- recontain.sh
+++ recontain.sh
@@ -1,13 +1,13 @@
 # Update stager and docker
 #echo 'Deleting Old Docker'
 #docker images -a | grep 'my-shiny-app' | awk '{print $3}' | xargs docker rmi -f
-#echo 'Grabbing New Data'
-#python3 getDatafd.py
+echo 'Grabbing New Data'
+~/anaconda3/envs/staging/bin/python3 getDatafd.py
 echo 'Recontainerising Data'
-docker build -t my-shiny-app . 
-docker save 'my-shiny-app' > my-shiny-app.tar
+docker build -t staging . 
+docker save 'staging' > staging.tar
 echo 'Loading Docker'
-docker load --input my-shiny-app.tar
+docker load --input staging.tar
 
 # Run With
-# docker run -p 3838:3838 my-shiny-app  
+# docker run -p 3838:3838 staging  
