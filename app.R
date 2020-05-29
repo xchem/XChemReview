48cfba1253960a9811163c67bec80ab7215a08a0
TJGorrie <tyler.gorrie-stone@diamond.ac.uk>
2020-05-29 13:14:09 +0100

    I need to reorganise the code again

--- app.R
+++ app.R
@@ -157,7 +157,7 @@ colss <- c(
     'Protein'
 )
 
-
+if(!local) source('/dls/science/users/mly94721/xchemreview/db_config.R') # Config file...
 con <- dbConnect(RPostgres::Postgres(), dbname = db, host=host_db, port=db_port, user=db_user, password=db_password)
 refinement_data <- dbGetQuery(con, "SELECT id, crystal_name_id, r_free, rcryst, ramachandran_outliers, res, rmsd_angles, rmsd_bonds, lig_confidence_string, cif, pdb_latest, mtz_latest FROM refinement WHERE outcome=4 OR outcome=5")
 crystal_data <- dbGetQuery(con, sprintf("SELECT id, crystal_name, compound_id, target_id FROM crystal WHERE id IN (%s)", paste(refinement_data[,'crystal_name_id'], collapse=',')))
