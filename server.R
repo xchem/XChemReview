2a51e1f060f8d13d0363046ac20bcc960ba893e5
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2020-09-30 09:38:14 +0100

    Updating boxsize to 5, 8 was too big!

--- server.R
+++ server.R
@@ -335,7 +335,7 @@ If you believe you have been sent this message in error, please email tyler.gorr
     loadDefaultParams <- function(){
         list(fogging=c(45,58),
             clipping=c(47,100),
-            boxsize=8,
+            boxsize=5,
             clipDist=5,
             backgroundColor='black',
             cameraType='orthographic',
