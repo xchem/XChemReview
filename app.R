b9906820c08c77daed1de19662e9c1c3969884dc
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2021-03-09 14:54:19 +0000

    Fix FS again

--- app.R
+++ app.R
@@ -190,7 +190,7 @@ createFragUploadFolder <- function(meta, target, copymaps=FALSE){
 
     # Zip File
     zipf <- sprintf('%s/%s.zip', rootf, prot)
-    system(sprintf('zip -r %s %s', zipf, basef))
+    system(sprintf('cd %s && zip -r %s %s', rootf, zipf, protsuffix))
 
     downloadHandler(filename <- function() { paste(prot, "zip", sep=".")},
     content <- function(file) {
