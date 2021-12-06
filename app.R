4b3a8d43190a57b4966eeee84b18a9c1df7c470d
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2021-12-06 12:47:14 +0000

    fix launchpad stuff

--- app.R
+++ app.R
@@ -254,7 +254,7 @@ createUniqueMetaData <- function(db, host_db, db_port, db_user, db_password, tar
 
     md <- dbGetQuery(con, sprintf("SELECT * FROM meta_data WHERE ligand_name_id in (%s)", md_ids))
     prot <- target
-    md <- md[!(md$Site_Label == 'IGNORE' | is.na(md$Site_Label)),] # Might be a problem for later...
+    md <- md[!(md$site_Label == 'IGNORE' | is.na(md$site_Label)),] # Might be a problem for later...
     meta <- md[grep(paste0(prot, '-'), md$fragalysis_name),c(6,7,3,3,4,2,5)]
     colnames(meta) <- c('crystal_name', 'RealCrystalName', 'smiles', 'new_smiles', 'alternate_name', 'site_name', 'pdb_entry')
 
