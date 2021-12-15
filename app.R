603aae249915a5c3a287f586a92a2c86dc1ae351
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2021-12-15 16:19:47 +0000

    o

--- app.R
+++ app.R
@@ -2062,7 +2062,7 @@ If you believe you have been sent this message in error, please email tyler.gorr
             #    }
             #}, deleteFile=FALSE)
             ligfile <- tail(dir(sprintf('%s/compound', isolate(sessionlist$xtalroot)), pattern = '.png', full.names=T),1)
-            renderedligfile <- tail(dir(dirname(isolate(sessionlist$apo_file)), pattern='png', full=T),1)
+            renderedligfile <- gsub('.mol', '.png', isolate(sessionlist$lig_mol_file))
             debugMessage(sID=sID, sprintf('%s', renderedligfile))
             output$rlimage <- renderImage({
                 if(length(renderedligfile) == 1){
