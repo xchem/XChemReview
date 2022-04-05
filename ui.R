f184f1069ba4324c75709fd7c99e72dc0f8f3c1b
Tyler Gorrie-Stone <tyler.gorrie-stone@diamond.ac.uk>
2022-04-05 11:55:02 +0100

    bf

--- ui.R
+++ ui.R
@@ -208,8 +208,8 @@ body <- dashboardBody(
             tabName = 'config',
             h2('N.B. Fiddling with this will trigger changes to how data will look in XCR - please give 24 hours after changing parameters'),
             selectInput('config_target', 'Target', choices=c('')),
-            checkboxInput('monomeric', 'Do not run PISA/Gemmi convert on target?' value = FALSE),
-            checkboxInput('reduce', 'Reduce Reference Frame to single Chain (usually Chain A)' value = TRUE),
+            checkboxInput('monomeric', 'Do not run PISA/Gemmi convert on target?', value = FALSE),
+            checkboxInput('reduce', 'Reduce Reference Frame to single Chain (usually Chain A)', value = TRUE),
             checkboxInput('covalent', 'Convert covalently attached mol files', value = TRUE),
             checkboxInput('active', 'Actively keep data up to date', value = TRUE),
             actionButton('config_change', 'Submit')
